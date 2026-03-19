8fabd53b157cbfa6a912895d4ac2821add52c1c5
Christian Scott <genericemail1011@gmail.com>
2026-03-19 17:44:39 -0500

    upload movie_review_stub.py

---
+++ movie_review_stub.py
@@ -0,0 +1,42 @@
+"""
+ASSIGNMENT 9B: SPRINT 2 - FUNCTIONAL STUBS
+Project: Movie review list
+Developer: Christian Scott
+"""
+
+# Separate user files
+USER_FILE = "User.txt"
+
+
+def get_user():
+    # User selects to make new profile or select existing one
+    # TODO: prompt user to make selection
+    return "Christian Scott"
+
+
+def view_movie_list():
+    # TODO: Show list of movies in file
+    # TODO: Allow user to select movie and see details
+    pass
+
+
+def edit_movie_list():
+    # TODO: Show list of movies in file
+    # TODO: Allow user to select movie and pick detail to edit.
+    pass
+
+
+def add_movie():
+    # TODO: Allow user to add film to file
+    # TODO: User selects details to add to film.
+    pass
+
+
+def main():
+    print("Please choose an option.")
+    print("1. View movie list 2. Edit movie list 3. Add movie to list 4. Quit")
+    choice = input("")
+    # TODO: user choice links to defined function.
+    
+get_user()
+main()
